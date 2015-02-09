4f73d267f1d823bb9d204e31cacf65e1e35084d7
bp <peavlerb@indiana.edu>
2015-02-09 18:16:10 -0500

    EDIT : Removed most of the code from the Matrix class, there is now only a few methods (redueced_row_echelon_form, printMatrix, consistent, and _swapRow).

--- code/math/LinearAlgebra/src/Matrix/Matrix.py
+++ code/math/LinearAlgebra/src/Matrix/Matrix.py
@@ -4,18 +4,49 @@ class Matrix:
     def __init__(self, mat):
         self.mat = mat
 
-    def reduced_echelon_form(self):
-        for c in range(len(self.mat) - 1):
-            for r in range(c+1, len(self.mat)):
-                if self._non_zero(self.mat[r][c]):
-                    m = self.mat[c][c]
-                    n = self.mat[r][c]
-                    scalars = self._find_scalars(m, n)
-                    self._gaussian(c, r, scalars)
+    def reduced_row_echelon_form(self):
+        if not self.mat:
+            return
 
+        lead     = 0
+        rowCount = len(self.mat)
+        colCount = len(self.mat[0])
 
-    def row_reduced_echelon_form(self):
-        pass
+        for r in range(rowCount):
+            if lead >= colCount:
+                return
+
+            i = r
+            while self.mat[i][lead] == 0:
+                i += 1
+                if i == rowCount:
+                    i     = r
+                    lead += 1
+                    if colCount == lead:
+                        return
+
+            self._swapRow(i, r)
+
+            lv = self.mat[r][lead]
+            self.mat[r] = [ mrx / float(lv) for mrx in self.mat[r]]
+
+            for i in range(rowCount):
+                if i != r:
+                    lv = self.mat[i][lead]
+                    self.mat[i] = [ iv - lv*rv for rv, iv in zip(self.mat[r], self.mat[i])]
+
+            lead += 1
+
+    def printMatrix(self):
+        for i in range(len(self.mat)):
+            print("[ ", end="")
+            for j in range(len(self.mat[i])):
+                item = self.mat[i][j]
+                if item >= 0:
+                    print(str(item + 0) + ' ', end="")
+                else:
+                    print("\b" + str(item) + ' ', end="")
+            print("]")
 
     def consistent(self):
         consistent = True
@@ -34,49 +65,8 @@ class Matrix:
 
         return consistent
 
-    def _gaussian(self, row1, row2, scalars):
-        for i in range(len(self.mat[row1])):
-            tmp1 = self.mat[row1][i] * scalars[0]
-            tmp2 = self.mat[row2][i] * scalars[1]
-            self.mat[row2][i] = tmp1 + tmp2
-
-    def _pos(self, n):
-        if n > 0:
-            return n
-        else:
-            return -n
-
-    def _neg(self, n):
-        if n > 0:
-            return -n
-        else:
-            return n
-
-    def _find_scalars(self, m, n):
-        mScalar = 1
-        nScalar = 1
-        for i in range(self._neg(n), self._pos(n) + 1):
-            if m * i == -n:
-                mScalar = i
-
-        for i in range(self._neg(m), self._pos(m) + 1):
-            if n * i == -m:
-                nScalar = i
-
-        return (mScalar, nScalar)
+    def _swapRow(self, r1, r2):
+        self.mat[r1], self.mat[r2] = self.mat[r2], self.mat[r1]
 
 
-    def _non_zero(self, entry):
-        return entry != 0
-
-
-
-mat = Matrix([[1, 0, 2],
-              [-2, 1, 3],
-              [4, 0, 2]])
-
-print(mat.mat)
-mat.reduced_echelon_form()
-print(mat.mat)
-print(mat.consistent())
 
